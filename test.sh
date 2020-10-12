dfd5eecac77cac965de0985d25b8c3cb49b97fef
srikanth4424 <samsrikanth007@gmail.com>
2020-10-12 17:30:07 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo -e "it's working access key is $secrets.ACCESSKEY  secrt is $secrets.ACCESSKEY"
+echo -e "it's working access key is $(secrets.ACCESSKEY)  secrt is $(secrets.ACCESSKEY)"
 echo -e "[se]\naws_access_key_id=$adummy\naws_secret_access_key=$sdummy\nregion=ap-south-1\noutput=json" > ~/.aws/config
 cat ~/.aws/config
