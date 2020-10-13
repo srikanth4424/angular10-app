6f80d740736dd07a25b365eedc0c411cd10f097e
srikanth4424 <samsrikanth007@gmail.com>
2020-10-13 17:37:46 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ echo -e "$1 it's working access key is jkbjk"
 roles=$(aws iam list-roles --query 'Roles[?contains(RoleName, `Angulr`) == `true` && ends_with(RoleName, `$1`)].RoleName' --output text)
 echo "$roles"
 echo "Attaching policy"
-for role in `aws iam list-roles --query 'Roles[?contains(RoleName, `Angulr`) == `true` && ends_with(RoleName, `$1`)].RoleName' --output text`; do
+for role in $(aws iam list-roles --query 'Roles[?contains(RoleName, `Angulr`) == `true` && ends_with(RoleName, `$1`)].RoleName' --output text); do
   echo "$role"
   aws iam attach-role-policy --role-name $role  --policy-arn arn:aws:iam::aws:policy/AmazonEC2FullAccess --output text
 done
