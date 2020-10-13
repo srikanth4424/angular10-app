6be5b8a9eebe2e0a2a0655d173014f3f8b139da8
srikanth4424 <samsrikanth007@gmail.com>
2020-10-13 16:00:52 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-echo -e "it's working access key is $(secrets.ACCESSKEY)  secrt is $(secrets.ACCESSKEY)  jkbjk"
+echo -e "$1 it's working access key is $(secrets.ACCESSKEY)  secrt is $(secrets.ACCESSKEY)  jkbjk"
 echo -e "[se]\naws_access_key_id=$adummy\naws_secret_access_key=$sdummy\nregion=ap-south-1\noutput=json" > ~/.aws/config
 cat ~/.aws/config
+
+aws iam list-roles --query 'Roles[].RoleName' 
