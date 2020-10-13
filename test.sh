4f94d5003eb8ad5cd8b66d0739333f8e4a92bb65
srikanth4424 <samsrikanth007@gmail.com>
2020-10-13 17:28:04 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ echo -e "$1 it's working access key is jkbjk"
 
 
 roles=$(aws iam list-roles --query 'Roles[?contains(RoleName, `Angulr`) == `true` && ends_with(RoleName, `$1`)].RoleName' --output text)
-
+echo "$roles"
 echo "Attaching policy"
 for role in $roles; do
   echo "$role"
